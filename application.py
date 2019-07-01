6c0c281dc6b92816f602f30941a4fb4b3b179f70
Bibek Regmi <bbkregmi@vt.edu>
2019-06-30 20:36:29 -0400

    Created registeration ui and a git ignore file

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 import sys
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,13 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def index():
@@ -29,3 +36,9 @@ def index():
 @app.route("/register")
 def register():
     return render_template('register.html')
+
+@app.route("/register_user", methods=['POST'])
+def register_user():
+    username = request.form['username']
+    password = request.form['password']
+    confirmpassword = request.form['confirm-password']
