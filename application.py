d415445813c16493f465b36bb57dea69d21208d4
Bibek Regmi <bbkregmi@vt.edu>
2019-07-01 22:17:18 -0400

    Database setup working!

--- application.py
+++ application.py
@@ -21,14 +21,6 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(50), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
 @app.route("/")
 def index():
     return render_template('login.html')
@@ -42,3 +34,11 @@ def register_user():
     username = request.form['username']
     password = request.form['password']
     confirmpassword = request.form['confirm-password']
+    
+    preexisting_user = db.execute("SELECT * FROM \"user\" WHERE username=:val", {'val': username}).fetchall()
+    if len(preexisting_user) != 0:
+        print("User already exists!")
+    else:
+        print("User does not exist")
+    return render_template('register.html')
+
